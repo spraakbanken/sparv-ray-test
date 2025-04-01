4cd9d4fd614f1d1ff6f4f39b0da5e59ff0da1ca7
Maria Öhrman <maria.ohrman@gu.se>
2025-04-01 09:25:11 +0200

    example with zipped working-dir, could not get it to work

---
+++ sparv/run_ray.py
@@ -0,0 +1,18 @@
+import sys
+from ray.job_submission import JobSubmissionClient
+
+runtime_env = {
+  "pip": "./requirements.txt",
+  "working_dir": "working_dir.zip",
+  "env_vars": {
+      "SPARV_DATADIR": "."
+  }
+}
+
+client = JobSubmissionClient("http://127.0.0.1:8265")
+job_id = client.submit_job(
+    entrypoint="sparv --dir example_corpora/mini-swe run",
+    runtime_env=runtime_env
+)
+
+print(job_id)
